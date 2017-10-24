d9c7ebb394aa937f4a794b131c5417ec865771af
8bril <mancusoo@gmail.com>
2017-10-24 14:54:24 -0500

    ejercicio areArraySame

---
+++ README.md
@@ -0,0 +1,14 @@
+  /*
+
+                   # Pseudocdigo
+
+  1.- Crear una función que reciba 2 arreglos como paramentro
+  2-. Comprobar si la longitud de los arreglos es diferente
+  2.1.- Si los arreglos no son iguales regresar a falso
+  2.2.- Si tienen la misma longitud continuar el proceso.
+  3.- Comprobar uno a uno los valores del arreglo.
+  3.1.- Si no son iguales regresar false
+  3.2.- Si son iguales sigue comparando uno a uno
+  4.-  Regresamos un true.
+
+  */
---
+++ app.js
@@ -0,0 +1,29 @@
+/*var areArraysSame = function (array1, array2){ // creando función con parametros//
+
+  if (array1.length !== array2.length){ // comparando el array
+
+    for (var i=0; i< array2.length; i++){ //
+
+      if (array1[i] !== array2[i])
+        return false;
+
+    }
+
+    return true;
+
+  }
+
+  return false;
+}
+
+console.log(areArraysSame([5,8,1,1,1],[5,8,1,1,0]));*/
+
+
+var comparacion = function (array1, array2) {
+  document.write("función declarada es una prueba")
+}
+
+var arreglo1 = [1,2,3,4,5,6,7,8,9,0]
+var arreglo2 = [1,2,3,4,5,6,7,8,9,0]
+
+compracion(arreglo1, arreglo2);
